d90806ddffbebe4385b7310e914f9937e744e3be
j-pro2002 <junembuyi@gmail.com>
2024-08-21 02:05:39 +0200

    test

---
+++ users/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+#create User model
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    username = None
+
+    USERNAME_FIELD = 'email' # login w/ email, unique identifier.
+    REQUIRED_FIELDS = [] 
+
